f01e1c73b0416fa3ae42b2c9918304b2a698b103
James Hight <james@zavoo.com>
2012-12-27 12:58:25 -0500

    Removed build directory

--- build/Release/.deps/Release/big_array.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/big_array.node := ln -f "Release/obj.target/big_array.node" "Release/big_array.node" 2>/dev/null || (rm -rf "Release/big_array.node" && cp -af "Release/obj.target/big_array.node" "Release/big_array.node")
